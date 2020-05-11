dd216b00d68b118db603b937a0ff911262166416
jessynakyeonglee <leena@ssfs.org>
2020-05-12 04:38:35 +0900

    Combinatorics completed

--- src/Combinatorics.java
+++ src/Combinatorics.java
@@ -4,7 +4,13 @@ public class Combinatorics {
      * Returns the factorial of n, as described in part (a). */
     public static int factorial(int n)
     { /* to be implemented in part (a) */
+        int factorial = 0;
+        if (n==1)
+            factorial=1;
+        else
+            factorial = n*factorial(n-1);
 
+        return factorial;
     }
 
 
@@ -12,5 +18,15 @@ public class Combinatorics {
      * from n choices and prints the result, as described in part
      (b). */
     public static void numCombinations(int n, int r)
-    { /* to be implemented in part (b) */ }
+    { /* to be implemented in part (b) */
+        int ways = 0;
+
+        if (r < n)
+            ways = 0;
+        else
+            ways = factorial(n)/(factorial(r)*factorial(n-r));
+
+        System.out.println("There are "+ways+" ways of choosing "+r+ " items from "+n+" choices.");
+
+    }
 }
